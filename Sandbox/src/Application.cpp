8de6f7cac433be05aff78a352f8edd1f91c3c166
Luis Luettgens <luis.luett@googlemail.com>
2021-12-04 17:16:16 +0100

    finished prj setup - application is ready

---
+++ Sandbox/src/Application.cpp
@@ -0,0 +1,6 @@
+#include "Test.h"
+
+int main()
+{
+    Hazel::Print();
+}
---
+++ build/Hazel/src/CMakeFiles/HazelLib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HazelLib.dir/Test.cpp.o"
+  "libHazelLib.a"
+  "libHazelLib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HazelLib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
